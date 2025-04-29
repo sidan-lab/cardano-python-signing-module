41b723c94770352c028c337d5dd56a9ef3eecdbc
SIDANWhatever <wongkahinhinson@gmail.com>
2025-04-29 16:26:20 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-signing-module
+
 Python signing module, implementation in Rust, ported over to C++, then to Python.
--- build.sh
+++ build.sh
@@ -1,7 +1,47 @@
+# cargo build
+# cp -f target/debug/libsigner.a src/libsigner.a
+# cp -f $(realpath target/cxxbridge/signer/src/lib.rs.h) src/lib.rs.h
+# swig -c++ -python src/signer.i
+# cd src
+# python setup.py build_ext --inplace
+# cd ..
+
+#!/bin/bash
+# Cross-platform build script
+
+set -e
+
+# Detect OS
+case "$(uname -s)" in
+    Linux*)     OS=linux;;
+    Darwin*)    OS=macos;;
+    MINGW*)     OS=windows;;
+    MSYS*)      OS=windows;;
+    CYGWIN*)    OS=windows;;
+    *)          OS=unknown;;
+esac
+
+echo "Building for $OS..."
+
+# Build the Rust library
 cargo build
-cp -f target/debug/libsigner.a src/libsigner.a
-cp -f $(realpath target/cxxbridge/signer/src/lib.rs.h) src/lib.rs.h
+
+# Copy the built library to the correct location
+if [ "$OS" = "windows" ]; then
+    cp -f target/debug/signer.lib src/libsigner.a
+    # On Windows, the path might be different
+    cp -f "$(find target/cxxbridge -name "lib.rs.h")" src/lib.rs.h
+else
+    cp -f target/debug/libsigner.a src/libsigner.a
+    cp -f $(realpath target/cxxbridge/signer/src/lib.rs.h) src/lib.rs.h
+fi
+
+# Generate SWIG bindings
 swig -c++ -python src/signer.i
+
+# Build the Python module
 cd src
 python setup.py build_ext --inplace
 cd ..
+
+echo "Build completed successfully for $OS"
