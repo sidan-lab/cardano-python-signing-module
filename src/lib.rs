ea3d86193232cc00d771ac2ced4f1fa9c977a6d5
twwu123 <tszwaiwu.96@gmail.com>
2025-04-30 15:44:53 +0800

    use from str derivation path

--- src/lib.rs
+++ src/lib.rs
@@ -14,50 +14,22 @@ mod ffi {
 }
 
 fn new_mnemonic_signer(mnemonic_phrase: &str, derivation_path: &str) -> Box<Signer> {
-    let mut derivation_path_vec: Vec<&str> = derivation_path.split('/').collect();
-    // remove first element if it is "m"
-    if derivation_path_vec[0] == "m" {
-        derivation_path_vec.remove(0);
-    }
-    let derivation_path_vec_u32: Vec<u32> = derivation_path_vec
-        .iter()
-        .filter_map(|&s| {
-            if s.ends_with("'") {
-                let mut chars = s.chars();
-                chars.next_back(); // Remove the last character (')
-                                   // Parse the string to u32 and add 0x80000000 for hardening
-                Some(chars.as_str().parse::<u32>().unwrap() + 0x80000000)
-            } else {
-                s.parse::<u32>().ok()
-            }
-        })
-        .collect();
     let wallet = Wallet::new(whisky::WalletType::MnemonicWallet(MnemonicWallet {
         mnemonic_phrase: mnemonic_phrase.to_string(),
-        derivation_indices: whisky::derivation_indices::DerivationIndices(derivation_path_vec_u32),
+        derivation_indices: whisky::derivation_indices::DerivationIndices::from_str(
+            derivation_path,
+        ),
     }));
     let account = wallet.get_account().unwrap();
     Box::new(Signer { account })
 }
 
 fn new_bech32_signer(root_private_key: &str, derivation_path: &str) -> Box<Signer> {
-    let derivation_path_vec: Vec<&str> = derivation_path.split('/').collect();
-    let derivation_path_vec_u32: Vec<u32> = derivation_path_vec
-        .iter()
-        .filter_map(|&s| {
-            if s.ends_with("'") {
-                let mut chars = s.chars();
-                chars.next_back(); // Remove the last character (')
-                                   // Parse the string to u32 and add 0x80000000 for hardening
-                Some(chars.as_str().parse::<u32>().unwrap() + 0x80000000)
-            } else {
-                s.parse::<u32>().ok()
-            }
-        })
-        .collect();
     let wallet = Wallet::new(whisky::WalletType::RootKeyWallet(RootKeyWallet {
         root_key: root_private_key.to_string(),
-        derivation_indices: whisky::derivation_indices::DerivationIndices(derivation_path_vec_u32),
+        derivation_indices: whisky::derivation_indices::DerivationIndices::from_str(
+            derivation_path,
+        ),
     }));
     let account = wallet.get_account().unwrap();
     Box::new(Signer { account })
